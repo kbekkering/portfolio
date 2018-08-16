4457840eea93df6ca3fae6386c00319e70cf2426
Koen Bekkering <koenzelf@MacZelf.local>
2018-08-16 09:33:01 +0200

    add materialize theme and topnav

---
+++ public/js/app.js
@@ -0,0 +1,6 @@
+// Mobile Topnav
+document.addEventListener('DOMContentLoaded', function() {
+  let elems = document.querySelectorAll('.sidenav');
+  let options = {};
+  let instances = M.Sidenav.init(elems, options);
+});
