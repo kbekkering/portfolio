96e186782bbbbe3be2381f4637a57f67a94cf13c
Koen Bekkering <koenzelf@MacZelf.local>
2018-08-20 12:38:08 +0200

    add page: variable to index route

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 let router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', { page: 'index' });
 });
 
 module.exports = router;
