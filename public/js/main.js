ba5f8f093a06ccb91ea3d8c0fa0ac080713eb29a
Koen Bekkering <koenzelf@MacZelf.local>
2018-08-22 11:02:14 +0200

    set sidenav options

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,9 @@
 // Mobile Topnav
 document.addEventListener('DOMContentLoaded', function() {
   let elems = document.querySelectorAll('.sidenav');
-  let options = {};
+  let options = { 
+    edge: 'left',
+    draggable: true
+  };
   M.Sidenav.init(elems, options);
 });
