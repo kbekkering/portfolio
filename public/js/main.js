6ee1ce6e208026a8dd586699a4c0fda526b92680
Koen Bekkering <koenzelf@MacZelf.local>
2018-08-23 11:58:58 +0200

    display flash messages in toasts

--- public/js/main.js
+++ public/js/main.js
@@ -19,4 +19,18 @@ document.addEventListener('DOMContentLoaded', function() {
     coverTrigger: false
   };
   M.Dropdown.init(yearsDropdown, yearsOptions);
+
+  // toasts show flash messages
+  let toast = document.querySelectorAll('.toast-trigger');
+  if (toast.length > 0) {
+    let toastType = toast[0].classList[1];
+    let toastColor = '';
+    if (toastType === 'error') {
+      toastColor = 'red';
+    } else if (toastType === 'success') {
+      toastColor = 'blue';
+    }
+    
+    M.toast({ html: toast[0].innerHTML, classes: toastColor });
+  }
 });
